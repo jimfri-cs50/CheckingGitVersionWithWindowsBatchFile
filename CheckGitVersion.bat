0b37cbceb72010c27e9aa26fdeb30530ac310127
netdataprojects <jim@netdataprojects.com>
2025-05-03 17:12:34 -0400

    To show what it looks like when just editing part of a line I made up a future Git version.

--- CheckGitVersion.bat
+++ CheckGitVersion.bat
@@ -15,7 +15,7 @@ echo Your installed Git version is: %CURRENT%
 
 REM Optional: Set threshold version you consider "outdated"
 REM First commited to Github on 5/3/2025
-set "LATEST_VERSION=2.49.0"
+set "LATEST_VERSION=2.55.0"
 
 REM Compare version strings (simple starts-with match)
 echo Checking if you have the latest known version (%LATEST_VERSION%)...
